00d7f60cefbe48602168170b0fa570724ba769d2
unknown <librechat2514@gmail.com>
2019-04-08 16:03:49 +0800

    seperate task init from task manager

---
+++ Assets/Script/Task/MazeTask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeTask : Task {
+
+    public override List<TaskTarget> Init(Transform playerController)
+    {
+        List<TaskTarget> targetList = new List<TaskTarget>();
+
+        // build maze by map
+
+        // put target
+
+        // put landmarks
+
+        return targetList;
+    }
+}
