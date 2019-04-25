12e018cca3b0ad01ad164538382db3100b8e4bca
unknown <librechat2514@gmail.com>
2019-04-25 14:38:18 +0800

    start to build my recording system

---
+++ Assets/Script/Recorder/RecordData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class RecordData
+{
+    public String name;
+    public float time;
+    public float m0; //float[3][4]: transform matrix
+    public float m1;
+    public float m2;
+    public float m3;
+    public float m4;
+    public float m5;
+    public float m6;
+    public float m7;
+    public float m8;
+    public float m9;
+    public float m10;
+    public float m11;
+
+    // controller key input
+    // public VRControllerState_t controllerState;
+
+    // gaze position data
+}
---
+++ Assets/Script/Recorder/VRRecorder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System.Threading;
+using ThreadPriority = System.Threading.ThreadPriority;
+
+public class VRRecorder : MonoBehaviour {
+
+	
+}
