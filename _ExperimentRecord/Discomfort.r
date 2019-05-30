3db0265075160182067a7c61de2ecc3c2d67f199
librechat <librechat2514@gmail.com>
2019-05-30 10:36:50 +0800

    0530 experiment analysis

--- _ExperimentRecord/Discomfort.r
+++ _ExperimentRecord/Discomfort.r
@@ -59,9 +59,11 @@ Draw = function(conditionIndex=0, participantIndex=0, sessionIndex = 0,
                 printAverage=TRUE, groupWithSession=FALSE,gradientByAnotherFactor=FALSE){
   plot(NULL, xlim=c(0,20), ylim=c(1,10), type="l", xlab="", ylab="",
        cex.lab=fontsize, cex.axis=fontsize, cex.main=fontsize, cex.sub=fontsize, xaxt="n", yaxt="n")
-  title(xlab="Time (minute)", mgp=c(4.5, 2, 0), cex.lab=fontsize)
-  title(ylab="Discomfort Score", mgp=c(3.5, 1, 0), cex.lab=fontsize)
-  axis(1, at=seq(0,20,1), cex.axis=fontsize, mgp=c(3, 2, 0))
+  
+  title(xlab="Time (minute)", mgp=c(2, 2, 0), cex.lab=fontsize)
+  axis(1, at=seq(0,20,1), cex.axis=fontsize)
+  
+  title(ylab="Discomfort Score", mgp=c(2, 1, 0), cex.lab=fontsize)
   axis(2, at=seq(1,10,1), cex.axis=fontsize)
   
   #draw lines
@@ -109,17 +111,39 @@ Draw = function(conditionIndex=0, participantIndex=0, sessionIndex = 0,
   }
   
   #print legends
-  if(groupWithSession){
-    legend(x=0,y=-5,
-           legend=paste(rep(sessionNames,times=2),rep(c("","(Average)"),each=3),sep=" "),
-           col=rep(color, times=2), pch = rep(NA, times=6), lwd = rep(c(1,4),each=3),
-           cex=fontsize*0.8, y.intersp = 1, ncol=2, bty="n", xpd=TRUE)
-  } else {
-    #colorize condition
-    legend(x=0,y=-5,
-           legend=paste(rep(conditionNames,times=2),rep(c("","(Average)"),each=3),sep=" "),
-           col=rep(color, times=2), pch = rep(NA, times=6), lwd = rep(c(1,4),each=3),
-           cex=fontsize*0.8, y.intersp = 1, ncol=2, bty="n", xpd=TRUE)
+  groupNames = conditionNames
+  anotherNames = sessionNames
+  if(groupWithSession) {
+    groupNames = sessionNames
+    anotherNames = conditionNames
+  }
+  if(printAverage){
+    if(gradientByAnotherFactor){
+      legend(x=0, y=-3,
+             legend = c(paste(rep(groupNames,times=2),rep(c("","(Average)"),each=3),sep=" "),anotherNames),
+             col = c(rep(color, times=2),rep("#000000",times=3)),
+             pch = rep(NA, times=9),
+             lwd = c(rep(c(1,4),each=3),1,2,3),
+             cex=fontsize*0.8, y.intersp = 1, ncol=3, bty="n", xpd=TRUE)
+    } else {
+      legend(x=0,y=-3,
+             legend=paste(rep(groupNames,times=2),rep(c("","(Average)"),each=3),sep=" "),
+             col=rep(color, times=2), pch = rep(NA, times=6), lwd = rep(c(1,4),each=3),
+             cex=fontsize*0.8, y.intersp = 1, ncol=2, bty="n", xpd=TRUE)
+    }
+  }
+  else {
+    if(gradientByAnotherFactor){
+      legend(x=0,y=-3,
+             legend=c(groupNames,anotherNames),
+             col=c(color,rep("#000000",times=3)), pch = rep(NA, times=6), lwd = c(rep(1,times=3),1,2,3),
+             cex=fontsize*0.8, y.intersp = 1, ncol=2, bty="n", xpd=TRUE)
+    } else{
+      legend(x=0,y=-3,
+             legend=groupNames,
+             col=color, pch = rep(NA, times=3), lwd = 1,
+             cex=fontsize*0.8, y.intersp = 1, ncol=1, bty="n", xpd=TRUE)
+    }
   }
 }
 Draw()
